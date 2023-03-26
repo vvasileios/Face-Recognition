1fadfcd08f2b086157cab595a1bde1853f1dca69
vvasileios <vtvasileiadis@gmail.com>
2023-03-26 17:44:37 +0300

    visual changes

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ class App extends Component {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <ParticlesBg type="cobweb" bg={true} />
+        <ParticlesBg type="square" bg={true} />
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         { route === 'home'
           ? <div>
